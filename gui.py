09675ebfd359ecec070b8485b5abed13c570528d
Jonas Ruppert <rpprtjns@gmail.com>
2025-02-16 22:01:01 +0100

    [ADD] simulation drawing

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import random
 import time
+import sys
 
 import pygame
 
@@ -14,7 +15,8 @@ class GUI:
         self.width = grid_width*4 + 20
         self.height = grid_height * 4 + 50
         
-        self.render_surface = pygame.Surface((self.width, self.height))
+        # self.render_surface = pygame.Surface((self.width, self.height))
+        self.render_surface = pygame.Surface((1600, 1200))
         
         self.COLOUR = {
             'WHITE' : (255, 255, 255),
@@ -22,46 +24,28 @@ class GUI:
             'BLACK' : (000, 000, 000)
             }
         
-        self.window = pygame.display.set_mode((self.height, self.width))
+        # self.window = pygame.display.set_mode((self.height, self.width))
+        self.window = pygame.display.set_mode((1600, 1200))
         pygame.display.set_caption('Euclidean Fluid Simulation')
         
-    def draw_simulation(self, pressure_grid: list = [0 for i in range(100_000)], width: int = 50):
-        for index, pressure in enumerate(pressure_grid):
-            x, y = index//width, index %width
-            pygame.draw.rect(self.window, 
-                             (random.randint(0, 255),
-                              random.randint(0, 255),
-                              random.randint(0, 255)
-                              ),
-                            pygame.Rect(x*6, y*6, 5, 5))
-        pygame.display.update()
-            
-            
-            
-    def draw_limit_test(self):
-        rects = [(random.randint(0, self.width - 4), # X position
-                      random.randint(0, self.height - 4), # Y position
-                      random.randint(0, 255), # Red channel
-                      random.randint(0, 255), # Green channel
-                      random.randint(0, 255)) # Blue channel
-                     for _ in range(300*400)]
+    def draw_pressure_2(self, pressure_list:list[float], width:int):
+        for index, pressure in enumerate(pressure_list):
+            x = index%width * 4
+            y = index//width * 4
+            colour =(random.randint(0, 255), # Red channel
+                     random.randint(0, 255), # Green channel
+                     random.randint(0, 255)) # Blue channel)
 
-            # Draw all rectangles to the offscreen surface
-        for rect in rects:
-            x, y, r, g, b = rect
-            pygame.draw.rect(self.render_surface, (r, g, b), (x, y, 4, 4))
-                
-                
-        # Blit the offscreen surface to the screen
+            pygame.draw.rect(self.render_surface, colour, (x, y, 4, 4))
+        
         self.window.blit(self.render_surface, (0, 0))
-
-        # Update the display
-        pygame.display.flip()
+        pygame.display.flip()  
         
     def mainloop(self):
         running = True
         T = time.time()
         while running:
+        
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     print(f'User quit the game')
@@ -76,14 +60,22 @@ class GUI:
             
             
             # self.draw_simulation()
-            self.draw_limit_test()
-            print(f'Render time: {round((time.time()-T)* 1000, 2)}ms {round(1/(time.time()-T), 3)} FPS')
+            # self.draw_limit_test()
+            # self.draw_pressure([0 for _ in range(120_000)], 400)
+            self.draw_pressure_2([0 for _ in range(14_400)], 160)
+            
+            self.__show_fps((time.time() - T)/100)
             T = time.time()
             
     def quit(self):
         pygame.quit()
         
+    def __show_fps(self, delta_time:float):
+        sys.stdout.write(f'\rRender time: {round(delta_time* 1000, 2)}ms {round(1/delta_time, 3)} FPS')
+        sys.stdout.flush()
+        
         
 gui = GUI(60, 80)
 gui.mainloop()
 gui.quit()
+
