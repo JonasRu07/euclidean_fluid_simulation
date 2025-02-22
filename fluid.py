d652b81dd0be3fc934e05de2e791827953afff63
Jonas Ruppert <rpprtjns@gmail.com>
2025-02-22 16:27:33 +0100

    Prevent mishaps with outside touch

--- fluid.py
+++ fluid.py
@@ -12,13 +12,13 @@ class Fluid:
         self.height = height
         
         self.u_grid = [[0 for _ in range(self.width)] for __ in range(self.height+1)]
-        self.v_grid = [[0 for _ in range(self.width+1)] for __ in range(self.height)]
+        self._v_grid = [[0 for _ in range(self.width+1)] for __ in range(self.height)]
         
         self.directional_grid = [[Vector2(0, 0) for _ in range(self.width)] for __ in range(self.height)]
                      
     def solve_incompressible_2D_array(self):
-        aux_u_gird = copy.deepcopy(self.u_grid)
-        aux_v_gird = copy.deepcopy(self.v_grid)
+        aux_u_grid = copy.deepcopy(self.u_grid)
+        aux_v_grid = copy.deepcopy(self.__v_grid)
         for i in range(self.height):
             for j in range(self.width):
                 # print(i, j)
@@ -31,45 +31,58 @@ class Fluid:
                     divergence = \
                         + self.u_grid[i][j] \
                         - self.u_grid[i+1][j] \
-                        + self.v_grid[i][j] \
-                        - self.v_grid[i][j+1]
-                    aux_u_gird[i][j] -= self.u_grid[i][j]
-                    aux_u_gird[i+1][j] += divergence/3
-                    aux_v_gird[i][j] -= divergence/3
-                    aux_v_gird[i][j+1] += divergence/3
-                    x = self.v_grid[i][j] + self.v_grid[i][j+1]
+                        + self.__v_grid[i][j] \
+                        - self.__v_grid[i][j+1]
+                    aux_u_grid[i][j] -= self.u_grid[i][j]
+                    aux_u_grid[i+1][j] += divergence/3
+                    aux_v_grid[i][j] -= divergence/3
+                    aux_v_grid[i][j+1] += divergence/3
+                    x = self._v_grid[i][j] + self._v_grid[i][j+1]
                     y = self.u_grid[i][j] + self.u_grid[i+1][j]
-                    self.directional_grid[i][j] = Vector2(x=self.v_grid[i][j] + self.v_grid[i][j+1],
+                    self.directional_grid[i][j] = Vector2(x=self._v_grid[i][j] + self._v_grid[i][j+1],
                                                         y=self.u_grid[i][j] + self.u_grid[i+1][j])
                 
                 elif i == self.height-1:
                     divergence = \
                         + self.u_grid[i][j] \
                         - self.u_grid[i+1][j] \
-                        + self.v_grid[i][j] \
-                        - self.v_grid[i][j+1]
-                    aux_u_gird[i][j] -= divergence/3
-                    aux_u_gird[i+1][j] = self.u_grid[i+1][j]
-                    aux_v_gird[i][j] -= divergence/3
-                    aux_v_gird[i][j+1] += divergence/3
-                    x = self.v_grid[i][j] + self.v_grid[i][j+1]
+                        + self.__v_grid[i][j] \
+                        - self.__v_grid[i][j+1]
+                    aux_u_grid[i][j] -= divergence/3
+                    aux_u_grid[i+1][j] = self.u_grid[i+1][j]
+                    aux_v_grid[i][j] -= divergence/3
+                    aux_v_grid[i][j+1] += divergence/3
+                    x = self.__v_grid[i][j] + self.__v_grid[i][j+1]
                     y = self.u_grid[i][j] + self.u_grid[i+1][j]
-                    self.directional_grid[i][j] = Vector2(x=self.v_grid[i][j] + self.v_grid[i][j+1],
+                    self.directional_grid[i][j] = Vector2(x=self.__v_grid[i][j] + self._v_grid[i][j+1],
                                                         y=self.u_grid[i][j] + self.u_grid[i+1][j])
                 else:
                     divergence = \
                         + self.u_grid[i][j] \
                         - self.u_grid[i+1][j] \
-                        + self.v_grid[i][j] \
-                        - self.v_grid[i][j+1]
-                    aux_u_gird[i][j] -= divergence/4
-                    aux_u_gird[i+1][j] += divergence/4
-                    aux_v_gird[i][j] -= divergence/4
-                    aux_v_gird[i][j+1] += divergence/4
-                    x = self.v_grid[i][j] + self.v_grid[i][j+1]
+                        + self._v_grid[i][j] \
+                        - self._v_grid[i][j+1]
+                    aux_u_grid[i][j] -= divergence/4
+                    aux_u_grid[i+1][j] += divergence/4
+                    aux_v_grid[i][j] -= divergence/4
+                    aux_v_grid[i][j+1] += divergence/4
+                    x = self._v_grid[i][j] + self._v_grid[i][j+1]
                     y = self.u_grid[i][j] + self.u_grid[i+1][j]
-                    self.directional_grid[i][j] = Vector2(x=self.v_grid[i][j] + self.v_grid[i][j+1],
+                    self.directional_grid[i][j] = Vector2(x=self._v_grid[i][j] + self._v_grid[i][j+1],
                                                         y=self.u_grid[i][j] + self.u_grid[i+1][j])
                 
-                self.u_grid = aux_u_gird
-                self.v_grid = aux_v_gird
+                self.u_grid = aux_u_grid
+                self._v_grid = aux_v_grid
+
+
+    @property
+    def u_grid(self):
+        return self._u_grid
+    
+    @property
+    def v_grid(self):
+        return self._v_grid
+    
+    @property
+    def flow_directions(self):
+        return self.directional_grid
