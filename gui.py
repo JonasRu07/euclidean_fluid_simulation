564dcabeaf449ffa652bdddf53cab7a89b4462cd
Jonas Ruppert <rpprtjns@gmail.com>
2025-02-22 20:51:46 +0100

    Fix it, missing cls

--- gui.py
+++ gui.py
@@ -19,14 +19,14 @@ class Colour:
     LIGHTGREEN = (167,201,87)
     
     @classmethod
-    def hex_to_rgb(hex_colour: str) -> tuple[int, int, int]:
+    def hex_to_rgb(cls, hex_colour: str) -> tuple[int, int, int]:
         if hex_colour[0] == '#':
             hex_colour = hex_colour[1:]
         return (int(hex_colour[0:2], 16), int(hex_colour[2:4], 16), int(hex_colour[4:6], 16))
     
     @classmethod
-    def rgb_to_hex(rgb: tuple[int, int, int]) -> str:
-        return f'#{hex(rgb[0])}{hex(rgb[1])}{hex(rgb[2])}'
+    def rgb_to_hex(cls, rgb: tuple[int, int, int]) -> str:
+        return f'#{str(hex(rgb[0]))[2:]}{str(hex(rgb[1]))[2:]}{str(hex(rgb[2]))[2:]}'
 
 
 class SimulationGUI:
@@ -106,3 +106,41 @@ class SimulationGUI:
         print('GUI quitting.')
         pygame.quit()
 
+class Menu_GUI:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry('800x600+-500-0')
+        
+        #Width of the Simulation
+        self.label_sim_width = tk.Label(master=self.window,
+                                        text='Width of the sim:')
+        self.label_sim_width.place(x=20, y=20, width=120, height=20)
+        self.entry_sim_width = tk.Entry(master=self.window)
+        self.entry_sim_width.place(x=160, y=20, width=50, height=20)
+        self.entry_sim_width.bind('<FocusOut>', self.check_valid)
+        
+        #Height for the simulation
+        self.label_sim_height = tk.Label(master=self.window,
+                                        text='Height of the sim:')
+        self.label_sim_height.place(x=20, y=60, width=120, height=20)
+        self.entry_sim_height = tk.Entry(master=self.window)
+        self.entry_sim_height.place(x=160, y=60, width=50, height=20)
+        self.entry_sim_height.bind('<FocusOut>', self.check_valid)
+        
+    def check_valid(self, event):
+        #Sim width:
+        user_input: str = self.entry_sim_width.get()
+        if user_input.isnumeric():
+            self.entry_sim_width.configure(bg=Colour.rgb_to_hex(Colour.LIGHTGREEN))
+        else:
+            self.entry_sim_width.configure(bg=Colour.rgb_to_hex((Colour.LIGHTRED)))
+        #Sim height:
+        user_input: str = self.entry_sim_height.get()
+        if user_input.isnumeric():
+            self.entry_sim_height.configure(bg=Colour.rgb_to_hex(Colour.LIGHTGREEN))
+        else:
+            self.entry_sim_height.configure(bg=Colour.rgb_to_hex(Colour.LIGHTRED))
+    def frame(self):
+        self.window.mainloop()
+        
+        
