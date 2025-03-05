3448b1394c120abb98a718775facf1aca10f6315
Jonas Ruppert <rpprtjns@gmail.com>
2025-03-05 14:26:51 +0100

    Fixing little stuff with rendering

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 import random
 import tkinter as tk
+import time
+
 import pygame
 
 pygame.init()
@@ -30,21 +32,22 @@ class Colour:
 
 
 class SimulationGUI:
-    def __init__(self, grid_width: int, grid_height: int, action):
+    def __init__(self, grid_width: int, grid_height: int, get_visualize_data, action, fps):
+        self.get_visualize_data = get_visualize_data
+        self.action = action
+        self.print_fps = fps
         
         self.grid_width = grid_width
         self.grid_height = grid_height
-        self.action = action
+        self.size_per_square = 1200//self.grid_width
         
-        self.width = grid_width*4 + 20
-        self.height = grid_height * 4 + 50
-        
-        self.render_surface = pygame.Surface((self.width, self.height))
-        
-        self.window = pygame.display.set_mode((self.height, self.width))
+        self.width = grid_width*self.size_per_square + 40
+        self.height = grid_height*self.size_per_square + 40
+        self.window = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Euclidean Fluid Simulation')
         
     def draw_pressure(self, pressure_list:list[float], width:int):
+        surface = pygame.Surface((self.width, self.height))
         for index, pressure in enumerate(pressure_list):
             x = index%width * 4
             y = index//width * 4
@@ -52,12 +55,13 @@ class SimulationGUI:
                      random.randint(0, 255), # Green channel
                      random.randint(0, 255)) # Blue channel)
 
-            pygame.draw.rect(self.render_surface, colour, (x, y, 4, 4))
+            pygame.draw.rect(surface, colour, (x, y, 4, 4))
         
-        self.window.blit(self.render_surface, (0, 0))
+        self.window.blit(surface, (0, 0))
         pygame.display.flip()  
         
     def draw_smoke(self, smoke_list:list[float], width):
+        surface = pygame.Surface((self.width, self.height))
         for index, smoke_overlay in enumerate(smoke_list):
             x = index%width * 4
             y = index//width * 4
@@ -65,45 +69,52 @@ class SimulationGUI:
                       255*smoke_overlay,
                       255*smoke_overlay)
 
-            pygame.draw.rect(self.render_surface, colour, (x, y, 4, 4))
+            pygame.draw.rect(surface, colour, (x, y, 4, 4))
         
-        self.window.blit(self.render_surface, (0, 0))
+        self.window.blit(surface, (0, 0))
         pygame.display.flip()  
         
     def draw_dir(self, direction_list: list, width:int):
+        surface = pygame.Surface((self.width, self.height))
         l = []
         for i in direction_list:
             for ii in i:
                 l.append(ii)
         
         for index, vector in enumerate(l):
-            pygame.draw.line(self.render_surface,
-                             (127,127,127),
-                             (index%width * 20 + 50, index//width * 20 + 50),
-                             (index%width * 20 + vector.x*5 + 50, index//width * 20 + vector.y*5 + 50))
+            pygame.draw.line(surface,
+                             Colour.WHITE,
+                             (index%width *self.size_per_square + 20, index//width * self.size_per_square + 20),
+                             (index%width * self.size_per_square + vector.x + 20, index//width * self.size_per_square + vector.y + 20))
             
-        self.window.blit(self.render_surface, (0,0))
+        self.window.blit(surface, (0,0))
         pygame.display.flip()
-        self.render_surface.fill((127, 69, 42))
         
-    def frame(self):
-        self.window.fill(Colour.BLACK)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                print(f'User quit the simulation')
-                return(False)
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    print(f'User quit the simulation')
-                    return False 
-                else:                   
-                    print(f'User pressed key {chr(event.key)}.')
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                print(f'User pressed mouse button at {event.pos}')
-        return True
+    def loop(self):
+        run = True
+        while run:
+            T = time.time()
+            self.window.fill(Colour.BLACK)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print(f'\n User quit the simulation')
+                    run = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        print(f'\n User quit the simulation')
+                        run = False
+                    else:                   
+                        print(f'\n User pressed key {chr(event.key)}.')
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(f'\n User pressed mouse button at {event.pos}')
+            self.action()
+            self.draw_dir(self.get_visualize_data(), self.grid_width)
+            # self.draw_pressure([0 for _ in range(self.grid_height*self.grid_width)], self.grid_width)
+            self.print_fps(time.time()-T)
+        self.quit()            
             
     def quit(self):
-        print('GUI quitting.')
+        print('\n --> GUI quitting.')
         pygame.quit()
 
 
@@ -120,7 +131,7 @@ class Menu_GUI:
                                         text='Width of the sim:')
         self.label_sim_width.place(x=20, y=20, width=120, height=30)
         self.entry_sim_width = tk.Entry(master=self.window)
-        self.entry_sim_width.insert(tk.END, '40')
+        self.entry_sim_width.insert(tk.END, '160')
         self.entry_sim_width.place(x=160, y=20, width=50, height=30)
         self.entry_sim_width.bind('<FocusOut>', self.check_valid)
         
@@ -129,7 +140,7 @@ class Menu_GUI:
                                         text='Height of the sim:')
         self.label_sim_height.place(x=20, y=60, width=120, height=30)
         self.entry_sim_height = tk.Entry(master=self.window)
-        self.entry_sim_height.insert(tk.END, '30')
+        self.entry_sim_height.insert(tk.END, '90')
         self.entry_sim_height.place(x=160, y=60, width=50, height=30)
         self.entry_sim_height.bind('<FocusOut>', self.check_valid)
         
@@ -153,6 +164,7 @@ class Menu_GUI:
         
         # Key bindings
         self.window.bind('<Return>', self.start_simulation_call)
+        self.window.bind('<Escape>', lambda event: self.window.destroy())
         self.entry_sim_height.bind('<Any-Enter>', self.easy_focus)
         self.entry_sim_width.bind('<Any-Enter>', self.easy_focus)
         self.entry_sim_tps.bind('<Any-Enter>', self.easy_focus)
