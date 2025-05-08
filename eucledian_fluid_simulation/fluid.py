e97406f42f71172284df46bf131a89ce8ad3caf7
Jonas Ruppert <rpprtjns@gmail.com>
2025-05-09 01:28:41 +0200

    Added velocity calculations

--- eucledian_fluid_simulation/fluid.py
+++ eucledian_fluid_simulation/fluid.py
@@ -10,6 +10,8 @@ from objects import Rectangle, Circle
 class Fluid:
     def __init__(self, width: int, height: int, tps:int, objects:list):
         
+        self.delta_time = 1/tps
+        
         self.width = width + 2
         self.height = height + 2
         
@@ -36,55 +38,52 @@ class Fluid:
             
         self.apply_object_interaction()
                      
-    def solve_incompressible_2D_array(self):
-        aux_u_grid = copy.deepcopy(self.u_grid)
-        aux_v_grid = copy.deepcopy(self.v_grid)
-        for i in range(self.height):
-            for j in range(self.width):
-                
-                #Scalers
-                scaler_x_0 = self.scalars[i-1][j]
-                scaler_x_1 = self.scalars[i+1][j]
-                scaler_y_0 = self.scalars[i][j-1]
-                scaler_y_1 = self.scalars[i][j+1]
-                
-                scaler = scaler_x_0 + scaler_x_1 + scaler_y_0 + scaler_y_1
-                
-                if scaler == 0: continue
-                
-                divergence = \
-                            + self.u_grid[i][j] \
-                            - self.u_grid[i+1][j] \
-                            + self.v_grid[i][j] \
-                            - self.v_grid[i][j+1]
-                            
-                pressure = divergence / scaler
-                
-                aux_u_grid[i][j] -= pressure * scaler_x_0
-                aux_u_grid[i+1][j] += pressure * scaler_x_1
-                aux_v_grid[i][j] -= pressure * scaler_y_0
-                aux_v_grid[i][j+1] += pressure * scaler_y_1
-                
-                self.directional_grid[i][j] = Vector2(x=self.v_grid[i][j] + self.v_grid[i][j+1],
-                                                      y=self.u_grid[i][j] + self.u_grid[i+1][j])
-                
-        self.u_grid = aux_u_grid
-        self.v_grid = aux_v_grid
+    def advect_velocity_simple(self):
         
-        
+        new_u = copy.deepcopy(self.u_grid)
+        new_v = copy.deepcopy(self.v_grid)
+
+        # Up Down
+        for j in range(1, self.height):
+            for i in range(1, self.width - 1):
+
+                u_val = self.u_grid[j][i]
+                v_val = (self.v_grid[j-1][i] + self.v_grid[j][i]) * 0.5
+
+                i_from = i - int(np.sign(u_val))
+                j_from = j - int(np.sign(v_val))
+                i_from = max(0, min(self.width - 2, i_from))
+                j_from = max(0, min(self.height - 1, j_from))
+
+                new_u[j][i] = self.u_grid[j_from][i_from]
+                
+        # Left right
+        for j in range(1, self.height - 1):
+            for i in range(1, self.width):
+                u_val = (self.u_grid[j][i-1] + self.u_grid[j][i]) * 0.5
+                v_val = self.v_grid[j][i]
+
+                i_from = i - int(np.sign(u_val))
+                j_from = j - int(np.sign(v_val))
+                
+                i_from = max(0, min(self.width - 1, i_from))
+                j_from = max(0, min(self.height - 2, j_from))
+
+                new_v[j][i] = self.v_grid[j_from][i_from]
+
+        self.u_grid = new_u
+        self.v_grid = new_v
+            
     def apply_object_interaction(self):
         for obj in self.objects:
             for i in range(self.width+1):
                 for j in range(self.height+1):
                     if obj.occupies(i, j):
-                        self.scalars[i][j] = 0
-        
-    
+                        self.scalars[i][j] = 0        
                     
     def reset_wind_channel(self, height:int, size:int, power:float):
         for i in range(self.height-height-size, self.height-height):
             self.v_grid[i][0] = power
-            
                 
     def get_flow_dir(self): return self.directional_grid
     
